2c567686b4da2d2af0e490ecce9b4cd9a2efb946
emanuelerm <emanueler00@gmail.com>
2023-06-15 12:16:44 +0200

    create general scaffolding

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h3>prova quack</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import LandingPage from "./pages/LandingPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: LandingPage,
+    },
+  ],
+});
+
+export { router };
